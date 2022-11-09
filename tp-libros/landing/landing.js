c8e46e39c84ff645eb6f38b19f07def42b7573f8
VSerra12 <vicky.serra.24@gmail.com>
2022-11-09 20:37:38 -0300

    Nueva carpera con estilos universales

--- tp-libros/landing/landing.js
+++ tp-libros/landing/landing.js
@@ -1,31 +1,17 @@
-
-// ============= SCROLLING ANIMATIONS =================
-
-window.addEventListener('DOMContentLoaded', e => {
-	document.querySelector('.one').style.animationName = 'fade-in-a';
-})
-
-window.addEventListener('scroll', e => {
-	// console.log(parseInt(document.querySelector('.three').getBoundingClientRect()['top']), parseInt(innerHeight) + 200)
-	if (pageYOffset > 30) {
-		document.querySelector('header').style.top = (innerWidth < 600) ? '-50px' : '-45px';
-	} else {
-		document.querySelector('header').style.top = '0px';
-	}
-	// hecks whether the element is within the limits of the viewport or not
-	let fade = (element, animationName) => {
-		if (element.getBoundingClientRect()['top'] < innerHeight) 
-			element.style.animationName = animationName;
-	}; 
+function generateBook(e){
 	
-	Array.from(document.querySelectorAll('.block:not(.one):not(.two)')).forEach(block => fade(block, 'fade-in-a'));
-	Array.from(document.querySelectorAll('#block7 img')).forEach(image => fade(image, 'fade-in-a'));
-	fade(document.querySelector('#block4'), 'fade-in-b');
-	fade(document.querySelector('#block5'), 'fade-in-b');
-})
+}
 
-// ############ get book from db ###############
+function getFromJSON(){
 
-function generateBook(){
-	
+	// let book = {
+	// 	bookImg: "",
+	// 	bookTittle: "",
+	// 	bookAuthor: "",
+	// 	bookIsbn: "",
+	// 	bookGender: ""
+	// }
+	let book = JSON.parse()
+
+
 }
