d3d223ad9a66e78d18b9fd55f3ff33e7db176fe1
VSerra12 <vicky.serra.24@gmail.com>
2022-11-20 00:06:51 -0300

    javascript de login

--- tp-libros/login/login.js
+++ tp-libros/login/login.js
@@ -1,5 +1,14 @@
 function login() {
 
+    const url = "http://127.0.0.1:3000/book-club-api/src/associated";
+    // fetch(url)
+    //     .then(data => {
+    //         return data.json();
+    //     })
+    //     .then(post => {
+    //         console.log(post.title);
+    //     });
+
     let form = document.forms["form"];
     let fd = new FormData(form);
     let data = {};
@@ -8,7 +17,6 @@ function login() {
     }
     VALUE = JSON.stringify(data, null, 2);
     console.log(VALUE);
-    const url = "http://127.0.0.1:3000/book-club-api/src/associated";
     const myHeaders = new Headers();
     myHeaders.append('Content-Type', 'application/json');
     fetch()(url, {
@@ -19,15 +27,35 @@ function login() {
         body: VALUE,
     })
         .then(data => data.json())
-        .then(data => { console.log(data) })
+        .then(data => { 
+            if(data.body === '' ){
+
+            }else{
+                saveLocalStorage()
+            }
+         })
         .catch((err) => {
             console.error(err);
         })
 
 }
+function saveLocalStorage() {
 
-const response = async () =>{
-    const response = await fetch("http://127.0.0.1:3000/book-club-api/src/associated")
-    const data = await response.json()
+    let token = {
+        
+    }
 
+    partida.name1 = game.player1.value;
+    partida.name2 = game.player2.value;
+    partida.score1 = s1;
+    partida.score2 = s2;
+
+    localStorage.setItem("game" + gameNumber, JSON.stringify(partida));
 }
+
+// const response = async () => {
+//     const response = await fetch("http://127.0.0.1:3000/book-club-api/src/associated")
+//     const data = await response.json()
+
+// }
+
