69ffb90efab7fb173eeb3352ca03461f30721a18
Rhonal Gonzalez <rhonalgnzl@gmail.com>
2022-11-22 19:34:45 -0300

    book controller fixed

--- book-club-api/src/book/book.controller.ts
+++ book-club-api/src/book/book.controller.ts
@@ -6,15 +6,19 @@ import {
   Put,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import {
+  ApiBearerAuth,
   ApiInternalServerErrorResponse,
   ApiNotFoundResponse,
   ApiTags,
 } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { Book } from './book.entity';
 import { BookService } from './book.service';
 
+@ApiBearerAuth()
 @ApiTags('book')
 @Controller('book')
 export class BookController {
@@ -29,21 +33,37 @@ export class BookController {
   @ApiInternalServerErrorResponse({
     description: 'Internal server error',
   })
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: number) {
     return this.bookService.findById(id);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Get('catalogo/')
+  findAllFilter() {
+    return this.bookService.findAllFilter();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('myBooks/:id')
+  findAllFilterId(@Param('id') id: number) {
+    return this.bookService.findAllFilterId(id);
+  }
+
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() body: Book) {
     return this.bookService.create(body);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
   update(@Param('id') id: number, @Body() body: Book) {
     return this.bookService.update(id, body);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   delete(@Param('id') id: number) {
     return this.bookService.remove(id);
