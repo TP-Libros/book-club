95b2c94652906571d8c45642681eb561b307d1a8
VSerra12 <vicky.serra.24@gmail.com>
2022-11-24 20:46:27 -0300

    funciona el login

--- tp-libros/login/login.js
+++ tp-libros/login/login.js
@@ -1,5 +1,6 @@
-async function login() {
-
+document.addEventListener("submit", (event) => {
+    event.preventDefault();
+    event.stopPropagation();
     const url = "http://localhost:3000/auth/login";
     let form = document.forms["form"];
     let fd = new FormData(form);
@@ -7,34 +8,78 @@ async function login() {
     for (let [key, prop] of fd) {
         datas[key] = prop;
     }
-    VALUE = JSON.stringify(datas, null,2);
+    VALUE = JSON.stringify(datas, null, 2);
     console.log(VALUE)
 
     const send = {
-    method: 'POST',
-    body: VALUE,
-    headers: {
-    'Content-Type': 'application/json',
-    }
+        method: 'POST',
+        body: VALUE,
+        headers: {
+            'Content-Type': 'application/json',
+        }
     };
 
-    await fetch(url, send)
+    fetch(url, send)
         .then(response => response.json())
-        .then(data => { 
-            
-            if(data.body === 'username or password incorrect' ){
+        .then(data => {
+
+            if (data.body === 'username or password incorrect') {
                 document.getElementById("errorMsg").appendChild("User or password incorrect")
-                window.location.href="login.html";
-            }else{
+                window.location.href = "login.html";
+            } else {
                 saveLocalStorage(data)
-                window.location.href="../libro_propio/libros_propios.html";
+                window.location.href = "../libro_propio/libros_propios.html";
             }
-         })
+        })
         .catch((err) => {
             console.error(err);
         })
 
 }
+)
+
+// function login(event) {
+
+
+//     const url = "http://localhost:3000/auth/login";
+//     let form = document.forms["form"];
+//     let fd = new FormData(form);
+//     let datas = {};
+//     for (let [key, prop] of fd) {
+//         datas[key] = prop;
+//     }
+//     VALUE = JSON.stringify(datas, null, 2);
+//     console.log(VALUE)
+
+//     const send = {
+//         method: 'POST',
+//         body: VALUE,
+//         headers: {
+//             'Content-Type': 'application/json',
+//         }
+//     };
+
+//     // let res =  fetch(url, send), 
+//     // json =  res.json();
+//     // console.log(json)
+
+//     fetch(url, send)
+//         .then(response => response.json())
+//         .then(data => {
+
+//             if (data.body === 'username or password incorrect') {
+//                 document.getElementById("errorMsg").appendChild("User or password incorrect")
+//                 window.location.href = "login.html";
+//             } else {
+//                 saveLocalStorage(data)
+//                 window.location.href = "../libro_propio/libros_propios.html";
+//             }
+//         })
+//         .catch((err) => {
+//             console.error(err);
+//         })
+
+// }
 function saveLocalStorage(data) {
     let token = data.token;
     let user = data.associated;
