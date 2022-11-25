607b1d43148b3633da618a406ba22c11af243110
Rhonal Gonzalez <rhonalgnzl@gmail.com>
2022-11-25 15:22:49 -0300

    book/id shows borrowing and associated, book/myBooks/id show all the associated books

--- book-club-api/src/book/book.module.ts
+++ book-club-api/src/book/book.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BookService } from './book.service';
 import { BookController } from './book.controller';
 import { Book } from './book.entity';
+import { Borrowing } from 'src/borrowing/borrowing.entity';
+import { BorrowingService } from 'src/borrowing/borrowing.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Book])],
-  providers: [BookService],
+  imports: [TypeOrmModule.forFeature([Book, Borrowing])],
   controllers: [BookController],
+  providers: [BookService],
 })
 export class BookModule {}
