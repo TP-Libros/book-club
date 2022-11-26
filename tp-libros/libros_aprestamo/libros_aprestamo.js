78812ed73c1e3cdc65dda5459d5facf4df5a034e
barbara <barbarasharon.diaz17@gmail.com>
2022-11-26 15:13:34 -0300

    actualizacion libros a prestamo

--- tp-libros/libros_aprestamo/libros_aprestamo.js
+++ tp-libros/libros_aprestamo/libros_aprestamo.js
@@ -1,74 +1,175 @@
 
 
-const d=document,
-$table=d.querySelector(".b-list"),
-$template=d.getElementById("template-book").content,
-$fragment=d.createDocumentFragment();
+window.onload = () => { cargeBook() }
+
+const loadBooks = (data) => {
+    let books = data[0];
+
+    data.forEach(el => {
 
 
-function getLocalStorage(){
-    let token;
-if(localStorage.getItem("TokenUser") === "undefined" || localStorage.getItem("TokenUser") === null){
-    window.location.href = '../login/login.html';
-}else{
-    token = JSON.parse(localStorage.getItem("TokenUser"));
+         let tittle = document.createElement("div")
+        tittle.classList.add("book-tittle")
+
+        tittleText = document.createElement("p")
+        tittleText = el.boo_id.boo_title
+        tittle.append(tittleText)
+
+        let author = document.createElement("div")
+        author.classList.add("book-author")
+        authorText = document.createElement("p")
+        authorText = el.boo_id.aut_id.aut_name + " " + el.boo_id.aut_id.aut_surname
+        author.append(authorText)
+
+       let image = document.createElement("div")
+        image.classList.add("book-img")
+        let imagesrc = document.createElement("img")
+        imagesrc.classList.add("image-b");
+        imagesrc.src = el.boo_id.boo_imagePath
+        image.append(imagesrc)
+
+        let isbn = document.createElement("div")
+        isbn.classList.add("book-isbn")
+        let isbnText = document.createElement("p")
+        isbnText = el.boo_id.boo_ISBN;
+        isbn.append(isbnText)
+
+        let gender = document.createElement("div")
+        gender.classList.add("book-gender")
+        let genderText = document.createElement("p")
+        genderText = el.boo_id.gen_id.gen_name
+        gender.append(genderText)
+
+        let fecha = document.createElement("div")
+        gender.classList.add("book-gender")
+        let fechaText = document.createElement("p")
+        fechaText = el.bor_from_date
+        fecha.append(fechaText)
+
+        let newBook = document.createElement("div");
+        newBook.classList.add("book");
+        newBook.setAttribute("id", data.boo_id)
+
+        let buttonVer = document.createElement("button")
+            buttonVer.classList.add("button")
+            buttonVer.textContent = "Ver"
+            buttonVer.setAttribute("onclick", "redirectSelectedModification(this.parentNode)")
+
+
+        newBook.append(image)
+        newBook.append(tittle)
+        newBook.append(author)
+        newBook.append(isbn)
+        newBook.append(gender)
+        newBook.append(fecha)
+        newBook.append(buttonVer)
+        
+        
+
+        document.getElementById("book-list").append(newBook)
+
+    });
+   
+
 }
-return token;
+
+function redirectSelectedModification(e){
+
+    localStorage.setItem("book", JSON.stringify(e.id))
+    window.location.href="../libro_seleccionado/libro_seleccionado.html"
 }
 
-function checkStatus(e){
-if (e.statusCode === 401) {
-    window.location.href = '../login/login.html';
-}
-}
 
 
-function getIdUser() {
+function checkUser() {
+    let token = getLocalStorage();
+    const url = "http://localhost:3000/associated";
 
-    let id;
-    if(localStorage.getItem("TokenUser") === "undefined" || localStorage.getItem("TokenUser") === null){
-        window.location.href = '../login/login.html';
-    }else{
-        id = JSON.parse(localStorage.getItem("User"));
-    }
-    return id.ass_id;
-}
 
-const getAll=async () => {
-    const urlLibros = "http://localhost:3000/borrowing/ass/"+getIdUser();
     const send = {
         method: 'GET',
         headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + getLocalStorage()
+            'Authorization': 'Bearer ' + token
         }
     };
-    try{
-        let res= await fetch(urlLibros,send),
-        json=await res.json();
-        checkStatus(res);
-        json.forEach(el => {
-            $template.querySelector(".image").src=el.boo_imagePath;
-            $template.querySelector(".title-book").textContent=el.boo_id.boo_title;
-            $template.querySelector(".isbn-book").textContent=el.boo_id.boo_ISBN;
-            $template.querySelector(".autor-book").textContent=el.boo_id.aut_id.aut_name+" "+el.boo_id.aut_id.aut_surname;
-            $template.querySelector(".gender-book").textContent=el.boo_id.gen_id.gen_name;
-            $template.querySelector(".fecha-book").textContent=el.bor_from_date;
 
-            jQuery(this).prev("td").attr("id")=el.boo_id;
-            
-            let $clone=d.importNode($template,true);
-            $fragment.appendChild($clone);
-        });
-
-        $table.querySelector("tbody").appendChild($fragment);
-        
-    } catch(err){
-        let message=err.statusText || "ERROR";
-        $table.insertAdjacentHTML("afterend",`<p><b>Error ${err.status}</b></p>`);
-
-    }
-   
+    fetch(url, send)
+        .then(res => checkStatus(res))
+        .catch((err) => {
+            console.error(err);
+        })
 }
 
-d.addEventListener("DOMContentLoaded",getAll);
+async function cargeBook() {
+
+    checkUser()
+
+    user = JSON.parse(localStorage.getItem("User"));
+
+    const url = "http://localhost:3000/borrowing/ass/" + user.ass_id
+
+    const send = {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + getLocalStorage()
+        }
+    }
+
+    try {
+        let response;
+        response = await fetch(url, send)
+        checkStatus(response)
+        let data = await response.json();
+        loadBooks(data)
+    } catch (e) {
+        return e.message;
+    }
+
+
+}
+
+
+async function sendRequest(url) {
+
+    var a = document.getElementById("book-list");
+
+    while (a.hasChildNodes())
+        a.removeChild(a.firstChild);
+
+    const send = {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + getLocalStorage()
+        }
+    }
+
+    try {
+        let response;
+        response = await fetch(url, send)
+        checkStatus(response)
+        let data = await response.json();
+        loadBooks(data)
+    } catch (e) {
+        return e.message;
+    }
+
+}
+
+
+function getLocalStorage() {
+    let token;
+    if (localStorage.getItem("TokenUser") === "undefined" || localStorage.getItem("TokenUser") === null) {
+        window.location.href = '../login/login.html';
+    } else {
+        token = JSON.parse(localStorage.getItem("TokenUser"));
+    }
+    return token;
+}
+
+function checkStatus(e) {
+    if (e.status === 401) {
+        window.location.href = '../login/login.html';
+    }
+}
+
+
